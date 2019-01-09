58c1f96ff68712e128dd0321b3643efc7df26099
Yorrd <yorrd.adornis@gmail.com>
2019-01-09 12:12:22 +0100

    depend on typescript

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'adornis:typescript',
-  version: '0.9.2',
+  version: '0.9.3',
   summary: 'TypeScript for Meteor',
   git: 'https://github.com/barbatus/typescript',
   documentation: 'README.md',
@@ -10,13 +10,14 @@ Package.registerBuildPlugin({
   name: 'typescript',
   use: ['adornis:typescript-compiler'],
   sources: ['plugin.js'],
+  npmDependencies: { typescript: '3.2.2' },
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.4.1');
 
   api.use('isobuild:compiler-plugin@1.0.0');
-  api.use('adornis:typescript-compiler@0.12.2');
+  api.use('adornis:typescript-compiler@0.12.3');
 
   api.imply('modules@0.11.6');
 
