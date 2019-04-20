9f0899de722f59f3623f15ef57dad0dff2fd5aa4
Yorrd <yorrd.adornis@gmail.com>
2019-04-20 14:43:44 +0200

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'adornis:typescript',
-  version: '0.9.11',
+  version: '0.9.12',
   summary: 'TypeScript for Meteor, based on barbatus:typescript',
   git: 'https://github.com/Adornis/typescript',
   documentation: 'README.md',
@@ -19,7 +19,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.4.1');
 
   api.use('isobuild:compiler-plugin@1.0.0');
-  api.use('adornis:typescript-compiler@0.12.11');
+  api.use('adornis:typescript-compiler@0.12.12');
 
   api.imply('modules@0.11.6');
 
