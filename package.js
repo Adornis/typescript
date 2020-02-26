77dea89ac32988ebf86de9aa2adceb0378e21f0c
Per Bergland <per@refapp.se>
2020-02-26 09:04:47 +0100

    upgrade TS to 3.8.2, bump versions

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'adornis:typescript',
-  version: '0.9.15',
+  version: '0.9.16',
   summary: 'TypeScript for Meteor, based on barbatus:typescript',
   git: 'https://github.com/Adornis/typescript',
   documentation: 'README.md',
@@ -11,7 +11,7 @@ Package.registerBuildPlugin({
   use: ['adornis:typescript-compiler'],
   sources: ['plugin.js'],
   npmDependencies: {
-    typescript: process.env.TYPESCRIPT_EXTERNAL_PATH ? 'file://' + process.env.TYPESCRIPT_EXTERNAL_PATH : '3.7.2',
+    typescript: process.env.TYPESCRIPT_EXTERNAL_PATH ? 'file://' + process.env.TYPESCRIPT_EXTERNAL_PATH : '3.8.2',
   },
 });
 
@@ -19,7 +19,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.4.1');
 
   api.use('isobuild:compiler-plugin@1.0.0');
-  api.use('adornis:typescript-compiler@0.12.15');
+  api.use('adornis:typescript-compiler@0.12.16');
 
   api.imply('modules@0.11.6');
 });
